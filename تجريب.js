8e7a8842bd259ac7e892fa537dca0fd5cd8ef9ee
Your Name <you@example.com>
2023-08-29 17:19:28 +0300

    Zad Acadamy

---
+++ تجريب.js
@@ -0,0 +1,4 @@
+ var id180=[ 'esraa','ahmed','ali'];
+ document.getElementById("novil").innerHTML=id180[1]+"  "+id180[2];
+ var rase={ first:'esraa',second:'ggg',third:'mmmmmm'};
+ document.getElementById('novil').innerHTML=rase.first;
